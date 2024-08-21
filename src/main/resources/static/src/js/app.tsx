7e27b5fb8c610bbc1c338526ea426eef8c4bd3d6
Gregg N <greggnobga@gmail.com>
2024-08-21 12:12:53 +0800

    Add react.

---
+++ src/main/resources/static/src/js/app.tsx
@@ -0,0 +1,13 @@
+/** Main component. */
+const App = () => {
+    console.log('Hello world!');
+    alert('hello world!');
+
+    return (
+        <section class='bg-green-500'>
+            <h1 className='text-3xl font-bold underline'>From react components!</h1>
+        </section>
+    );
+};
+
+export default App;
